90c188d1ffd46e6d504ae27b9c3f7e1523430cf6
luisgambetta <luis.gambetta1@gmail.com>
2019-12-11 15:19:39 +0100

    initialize scenario

---
+++ part3_(bce).py
@@ -0,0 +1,20 @@
+import numpy as np
+import random
+from scipy.linalg import eig 
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+#Length of steps
+T = 1000
+
+#Transition matrix
+IntruderT = np.array([[0,1.0,0,0,0,0,0,0,0,0],
+                      [0.5,0,0.5,0,0,0,0,0,0,0],
+                      [0,1/3,0,1/3,0,0,1/3,0,0,0],
+                      [0,0,1/2,0,1/2,0,0,0,0,0],
+                      [0,0,0,1/4,0,1/4,1/4,0,0,1/4],
+                      [0,0,0,0,1/2,0,0,0,1/2,0],
+                      [0,0,1/3,0,1/3,0,0,1/3,0,0],
+                      [0,0,0,0,0,0,1/2,0,1/2,0],
+                      [0,0,0,0,0,1/2,0,1/2,0,0],
+                      [0,0,0,0,1,0,0,0,0,0]])
